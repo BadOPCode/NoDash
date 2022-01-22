f3a3e6ef20ff89df7fa3c1f3c09924438496b725
Shawn Rapp <badopcode@hotmail.com>
2022-01-22 11:23:10 -0700

    Upgraded/removed deps. Fixed linting.

--- __tests__/Utils.test.ts
+++ __tests__/Utils.test.ts
@@ -6,29 +6,29 @@
  */
 "use strict";
 
-import { Expect, Test, TestFixture } from 'alsatian';
-import * as Utils from '../src/Utils';
+import { Expect, Test, TestFixture } from "alsatian";
+import * as Utils from "../src/Utils";
 
-@TestFixture('Utils Functions')
+@TestFixture("Utils Functions")
 export class FixtureUtils {
-    @Test('should not match if string to number')
+    @Test("should not match if string to number")
     public testFailStrToNum() {
-        const results = Utils.CheckTypesMatch('hello', 42);
+        const results = Utils.CheckTypesMatch("hello", 42);
         Expect(results).toBe(false);
     }
 
-    @Test('should match if both are arrays')
+    @Test("should match if both are arrays")
     public testPassArrToArr() {
         const x = [1,2,3];
         const y = ["a", "b", "c"];
         const results = Utils.CheckTypesMatch(x,y);
         Expect(results).toBe(true);
     }
-    
-    @Test('should not match if one is an array and the other is an object')
+
+    @Test("should not match if one is an array and the other is an object")
     public testFailArrToObj() {
         const x = [1,2,3];
-        const y = {"a":1, "b":2, "c":3};
+        const y = {a:1, b:2, c:3};
         const results = Utils.CheckTypesMatch(x,y);
         Expect(results).toBe(false);
     }
